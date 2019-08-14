896f40dd57b67771b5d11d3ed964baa34173b9ac
pkellner <peter@peterkellner.net>
2019-08-14 16:55:24 -0700

    first checkin

---
+++ TagHelperComponentApp/GoogleAnalyticsTagHelperComponent.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TagHelperComponentApp
+{
+    public class GoogleAnalyticsTagHelperComponent : TagHelperComponent
+    {
+        public override Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            if (string.Equals(context.TagName, "body", StringComparison.OrdinalIgnoreCase))
+            {
+                output.PostContent.AppendHtml("<script>console.log('Pretend this is google analytics code being injected by GoogleAnalyticsTagHelperComponent ');</script>");
+            }
+            return Task.CompletedTask;
+        }
+    }
+}
